a577adab102e2c7b0c3a18f1eb87c228e2ab7e28
harpyeagle-dev <55797063+harpyeagle-dev@users.noreply.github.com>
2025-03-31 09:28:37 -0400

    Update save_to_sheets.py

--- Modules/save_to_sheets.py
+++ Modules/save_to_sheets.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google.oauth2 import service_account
 import gspread
 
-def save_vote_to_sheets(vote_data):
+def save_votes_to_sheets(vote_data):
     scope = ["https://www.googleapis.com/auth/spreadsheets"]
     credentials = service_account.Credentials.from_service_account_info(
         st.secrets["google_service_account"],
